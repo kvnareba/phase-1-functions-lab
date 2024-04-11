1b2701a9b4a66bd794b8293c9ccc35b03837b035
kvn <kvnareba@gmail.com>
2024-04-11 08:49:41 +0300

    com

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(location) {
+    const headquarters = 42; 
+    return Math.abs(location - headquarters);
+}
+function distanceFromHqInFeet(location){
+  const feetPerBlock = 264; 
+const blocks = Math.abs(location - 42);
+return blocks * feetPerBlock;
+
+}
+
+
+function distanceTravelledInFeet(start, destination) {
+const feetPerBlock = 264; 
+const blocks = Math.abs(destination - start);
+return blocks * feetPerBlock;
+}
+
+function calculatesFarePrice(start, destination) {
+const feetPerBlock = 264;
+const distance = Math.abs(destination - start) * feetPerBlock;
+
+if (distance <= 400) {
+  return 0;
+}
+
+if (distance <= 2000) {
+  return (distance - 400) * 0.02;
+}
+
+if (distance > 2000 && distance <= 2500) {
+  return 25;
+}
+
+return 'cannot travel that far';
+}
